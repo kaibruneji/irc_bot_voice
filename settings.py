54ce2bfb209baa9ad43010d763e0b551915bdc14
Andrey Popov <42667044+kaibruneji@users.noreply.github.com>
2019-04-30 23:33:55 +0700

    start

---
+++ settings.py
@@ -0,0 +1,21 @@
+﻿network = 'irc.tambov.ru'
+port = 7770
+channel = '#magi'
+botName = 'defender'
+masterName = 'Кай'
+password = 'xxx'
+
+def settings(x):
+    if x == 'network':
+        return network
+    elif x == 'port':
+        return port
+    elif x == 'botName':
+        return botName
+    elif x == 'masterName':
+        return masterName
+    elif x == 'password':
+        return password   
+    elif x == 'channel':
+        return channel
+    
