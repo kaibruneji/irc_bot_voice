7bf9f83fb3a97c15f748557379ae6fac62978b75
Andrey Popov <42667044+kaibruneji@users.noreply.github.com>
2019-05-14 10:03:41 +0700

    add random questions

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 import time
 import requests
 import settings
+import dictvoice
 import sqlite3
 import random
 
@@ -40,19 +41,7 @@ message = ''
 num_users = 0
 
 dict_name_question = {}
-dict_voice_quest = {
-  'солнце':'Как называется звезда нашей солнечной системы?',
-  'луна':'Как называется естественный спутник планеты Земля?',
-  'восемь':'Сколько будет от десяти отнять четыре и потом прибавить два?\
-(Напишите буквами)',
-  'четыре':'Сколько у собаки лап?(напишите буквами)',
-  'красный':'Какой запрещающий цвет загорается на светофоре?'
-  }
-
-question_voice = 'Сколько будет от пятнадцати отнять шесть и прибавить единицу? \
-(напишите буквами!)'
-answer_voice = 'десять'
-answer_voice_upper = 'Десять'
+dict_voice_quest = settings.settings('dict_voice_quest')
 
 #-------Major_while-------------------------
 
@@ -68,7 +57,7 @@ while True:
     # Make variables Name, Message, IP from user message.
     if data.find('PRIVMSG') != -1:
         name = data.split('!',1)[0][1:]
-        message = data.split('PRIVMSG',1)[1].split(':',1)[1].strip()
+        message = data.split('PRIVMSG',1)[1].split(':',1)[1].strip().lower()
     try:
         ip_user=data.split('@',1)[1].split(' ',1)[0]
     except:
@@ -76,18 +65,22 @@ while True:
 
     #-----------GET num_users and give voice----------
     
-    if 'PRIVMSG '+channel+' :!NAMES\r\n' in data and name == masterName:
-        send('NAMES '+channel+'\r\n')
+    #if 'PRIVMSG '+channel+' :!NAMES\r\n' in data and name == masterName:
+     #   send('NAMES '+channel+'\r\n')
+
+    if ' QUIT :' in data or ' PART '+channel+' :' \
+       in data and 'PRIVMSG' not in data or 'PRIVMSG '+channel+' :!NAMES\r\n' in \
+       data and name == masterName:
+        send('NAMES '+channel+'\r\n')    
        
     if botName+' = '+channel+' :' in data and 'PRIVMSG' not in data:
         num_users = len(data.split(channel+' :')[1].split(' \r\n')[0].split(' '))
         num_users += 1
         num_users_str = str(num_users)        
         send('MODE '+channel+' +l '+num_users_str+'\r\n')        
-        print('I GOT USER NUM_LIST+1 : '+num_users_str+'\r\n')        
+        print('I GOT USER NUM_LIST+1 : '+num_users_str+'\r\n')
             
-    if ' JOIN :'+channel in data or ' QUIT :' in data or ' PART '+channel+' :' in data\
-       and 'PRIVMSG' not in data:
+    if ' JOIN :'+channel in data and 'PRIVMSG' not in data:
         name_join = data.split('!')[0].split(':')[1]
         conn = sqlite3.connect('users.db')
         c = conn.cursor()
@@ -98,27 +91,29 @@ while True:
         voice_table_for_work_2 = (voice_table_for_work,)
         send('NAMES '+channel+'\r\n')
         if in_name_table == None:
-            #question_voice = dict_voice_quest.get(random.choice(list(dict_voice_quest.keys())))
-            #dict_name_question[name_join] = random.choice(list(dict_voice_quest.keys()))
-            send('PRIVMSG '+name_join+' :'+question_voice+'\r\n')            
+            question_voice = random.choice(list(dict_voice_quest))
+            send('PRIVMSG '+name_join+' :'+question_voice+'\r\n')
+            dict_name_question[name_join] = dict_voice_quest[question_voice]
         elif in_name_table == voice_table_for_work_2:
             send('MODE '+channel+' +v '+name_join+'\r\n')
-
-    if 'PRIVMSG '+botName+' :'+answer_voice in data or \
-       'PRIVMSG '+botName+' :'+answer_voice_upper in data:        
-            name_in_table = (name,)
+            
+    if 'PRIVMSG '+botName+' :' and name in dict_name_question and \
+       message == dict_name_question[name]:        
+        name_in_table = (name,)
+        conn = sqlite3.connect('users.db')
+        c = conn.cursor()        
+        c.execute('SELECT * FROM stocks WHERE name=?', name_in_table)            
+        if c.fetchone() == None:
+            send('MODE '+channel+' +v '+name+'\r\n')
+            send('PRIVMSG '+name+' :Ответ правильный! Теперь у вас при заходе \
+на канал с этого ника будет автоматически давать +v (голос)\r\n')
             conn = sqlite3.connect('users.db')
-            c = conn.cursor()        
-            c.execute('SELECT * FROM stocks WHERE name=?', name_in_table)                
-            if c.fetchone() == None:
-                send('MODE '+channel+' +v '+name+'\r\n')
-                conn = sqlite3.connect('users.db')
-                c = conn.cursor()
-                purchse_users_db = [(name, 'voice')]
-                c.executemany('INSERT INTO stocks VALUES (?,?)', purchse_users_db)
-                conn.commit()
-                conn.close
-
+            c = conn.cursor()
+            purchse_users_db = [(name, 'voice')]
+            c.executemany('INSERT INTO stocks VALUES (?,?)', purchse_users_db)
+            conn.commit()
+            conn.close
+        
         # Out command.  
     if data.find('PRIVMSG '+channel+' :!defender quit') != -1 and name == masterName:
         send('PRIVMSG '+channel+' :Хорошо, всем счастливо оставаться!\r\n')
